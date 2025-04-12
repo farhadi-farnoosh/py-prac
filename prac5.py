483c68b88917304ebc69d56cb01b5ed4ae5c2826
farhadi-farnoosh <farhadi.farnush@outlook.com>
2025-04-13 02:17:52 +0330

    prac5.py

--- prac5.py
+++ prac5.py
@@ -1,32 +1,3 @@
-#for count the number of each proper noun
-# word = {}
-# text = input().split('. ')
-# text[-1] = text[-1][:-1]
-
-# for i in range(len(text)):
-#     text[i] = text[i].split(' ')
-#     del(text[i][0])
-#     text[i] = ' '.join(text[i])
-
-# text = ' '.join(text)
-# text = text.split()
-
-# for i in range(len(text)):
-#     if text[i].endswith(','):
-#         text[i] = text[i][:-1]
-
-# for i in text:
-#     if i.istitle():
-#         word[i] = 1
-
-# for i in word:
-#     word[i] = text.count(i)
-
-# if word == {}:
-#     print(None)
-# else:
-#      for key, value in word.items(): print(value, ':', key, sep='')
-
 word = []
 text = input().split('. ')
 text[-1] = text[-1][:-1]
